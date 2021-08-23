9ea4af30f4342abbf4d2cde8c37473385df97943
Lucas Cota <lucascdornelas@gmail.com>
2021-08-23 18:42:18 -0300

    wip: complete flow at functions fourier

--- filters.py
+++ filters.py
@@ -5,7 +5,6 @@ from skimage.color import rgb2gray
 from utils import *
 
 ## Gaussian Filters
-
 def distance(point1,point2):
     return math.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
 
